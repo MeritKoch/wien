d268f885f4a706bc5a80f397d6933728e9ba7f23
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-04-25 17:34:20 +0200

    Alle Busicons machen

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
 let themaLayer = {
-  sights: L.featureGroup().addTo(map),
+  sights: L.featureGroup(),
   lines: L.featureGroup(),
   stops: L.featureGroup().addTo(map),
   zones: L.featureGroup(),
@@ -168,13 +168,12 @@ async function loadStops(url) {
   // console.log("Loading", url);
   let response = await fetch(url)
   let geojson = await response.json();
-  console.log(feature);
   L.geoJSON(geojson, {
-
     pointToLayer: function (feature, latlng) {
+      console.log(feature);
       return L.marker(latlng, {
         icon: L.icon({
-          iconUrl: "icons/bus_1.png",
+          iconUrl: `icons/bus_${feature.properties.LINE_ID}.png`,
           iconAnchor: [16, 37],
           popupAnchor: [0, -37]
         })
